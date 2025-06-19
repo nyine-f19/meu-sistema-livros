c90ac96bb9b92534751e001e8c1800530ef874f5
nyine-f19 <adrianeliasfranco2018@gmail.com>
2025-06-19 02:32:24 -0300

    Removendo Inserção desnecessárias

--- api-ifbooks/src/main/resources/db/migration/V004__insere_livros.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO livro (titulo, data_publicacao, editora, resumo, autor_id) VALUES ('Harry Potter e a Pedra Filosofal', '1997-06-26', 'Rocco', 'Um menino descobre em seu décimo primeiro aniversário que ele é o filho órfão de dois bruxos poderosos e possui poderes mágicos únicos.', 1);
-
-INSERT INTO livro (titulo, data_publicacao, editora, resumo, autor_id) VALUES ('O Senhor dos Anéis: A Sociedade do Anel', '1954-07-29', 'Allen & Unwin', 'Um hobbit do Condado e oito companheiros partem em uma jornada para destruir o Um Anel e o Lorde das Trevas Sauron.', 2);

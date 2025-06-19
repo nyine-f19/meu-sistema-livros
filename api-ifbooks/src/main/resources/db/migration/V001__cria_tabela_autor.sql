452c100a0b49e9a691a3c129ea7a7991a6840c56
nyine-f19 <adrianeliasfranco2018@gmail.com>
2025-06-19 01:42:34 -0300

    Alteração dos ARQ sql

--- api-ifbooks/src/main/resources/db/migration/V001__cria_tabela_autor.sql
+++ api-ifbooks/src/main/resources/db/migration/V001__cria_tabela_autor.sql
@@ -1,7 +1,6 @@
 CREATE TABLE autor (
-    id bigint NOT NULL AUTO_INCREMENT,
-    nome varchar(80) NOT NULL,
-    data_nascimento date,
-    nacionalidade varchar(20) NOT NULL,
-    PRIMARY KEY (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+    id BIGSERIAL PRIMARY KEY,
+    nome VARCHAR(80) NOT NULL,
+    data_nascimento DATE,
+    nacionalidade VARCHAR(20) NOT NULL
+);
--- api-ifbooks/src/main/resources/db/migration/V002__insere_autores.sql
+++ api-ifbooks/src/main/resources/db/migration/V002__insere_autores.sql
@@ -2,4 +2,3 @@ insert into autor (nome, data_nascimento, nacionalidade) values
  ('Joanne Rowling', '1965-07-31', 'GBR');
 insert into autor (nome, data_nascimento, nacionalidade) values 
  ('John Ronald Reuel Tolkien', '1892-01-03', 'GBR');
-commit;
